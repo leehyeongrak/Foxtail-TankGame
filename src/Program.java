1dfc2421958acff6226db3a89fb05314ff69f6e1
chanhee13p <chanhee13p@gmail.com>
2017-11-18 22:10:20 +0900

    PApplet

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,47 @@
-public class Program {
+import processing.core.PApplet;
+
+public class Program extends PApplet {
+    int[][] map = new int[Constants.MAP_COL][Constants.MAP_ROW];
+
+    @Override
+    public void settings() {
+        this.size(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        makeMap();
+
+    }
+
+    @Override
+    public void draw() {
+    }
+
+    private void makeMap() {
+        map = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+                        {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+                        {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        };
+//        for(int y=0; y<Constants.MAP_COL; y++){
+//            for(int x=0; x<Constants.MAP_ROW; x++){
+//                System.out.print(map[y][x]);
+//            }
+//            System.out.println("");
+//        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("test");
+        PApplet.main("Program");
     }
 }
+
