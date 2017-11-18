9c2636ffef18c2796642454e88e21a43972c896f
chanhee13p <chanhee13p@gmail.com>
2017-11-19 06:21:00 +0900

    패킷처리

--- src/User.java
+++ src/User.java
@@ -11,42 +11,84 @@ public class User extends RenderObject {
     final static int STAY_UP = 63;
     final static int STAY_DOWN = 64;
 
+    final static int MOVE = 1;
+    final static int STOP = 0;
 
     String id;
+    int team = 0;
     int speed = 2;
     int dir;
+    int isMove = STOP;
+
+    public boolean disconnect = false;
+    public boolean willBeDestroy = false;
 
     User(PApplet pApplet, String id) {
         super(pApplet);
         this.id = id;
-        this.x = 40;
-        this.y = 40;
         this.mode = STAY;
     }
-    User(PApplet pApplet) {
+
+    User(PApplet pApplet, String id, String x, String y, String dir, String team) {
         super(pApplet);
-        this.x = 40;
-        this.y = 40;
-        this.mode = STAY;
-        id = Constants.ID;
+        this.x = (int) Double.parseDouble(x);
+        this.y = (int) Double.parseDouble(y);
+        this.id = id;
+        if (dir.equals("LEFT")) {
+            this.dir = Constants.DIR_LEFT;
+        } else if (dir.equals("RIGHT")) {
+            this.dir = Constants.DIR_RIGHT;
+        } else if (dir.equals("UP")) {
+            this.dir = Constants.DIR_UP;
+        } else if (dir.equals("DOWN")) {
+            this.dir = Constants.DIR_DOWN;
+        }
+        this.team = (int) Double.parseDouble(team);
     }
+
+    public User(PApplet applet, int team, int x, int y, String id) {
+        super(applet);
+        this.team = team;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+
     @Override
     public void update() {
-//        if (mode == MOVE_LEFT) {
-//            this.x -= speed;
-//        } else if (mode == MOVE_RIGHT) {
-//            this.x += speed;
-//        } else if (mode == MOVE_DOWN) {
-//            this.y += speed;
-//        } else if (mode == MOVE_UP) {
-//            this.y -= speed;
-//        } else if (mode == STAY) {
-//            return;
-//        }
+        if (isMove == STOP) return;
+        if (dir == Constants.DIR_LEFT) {
+            this.x -= speed;
+        } else if (dir == Constants.DIR_RIGHT) {
+            this.x += speed;
+        } else if (dir == Constants.DIR_DOWN) {
+            this.y += speed;
+        } else if (mode == Constants.DIR_UP) {
+            this.y -= speed;
+        } else if (mode == STAY) {
+            return;
+        }
     }
 
     public void setMode(int mode) {
         this.mode = mode;
     }
 
+    public void setDir(String dir) {
+        switch (dir) {
+            case "LEFT":
+                this.dir = Constants.DIR_LEFT;
+                break;
+            case "RIGHT":
+                this.dir = Constants.DIR_RIGHT;
+                break;
+            case "UP":
+                this.dir = Constants.DIR_UP;
+                break;
+            case "DOWN":
+                this.dir = Constants.DIR_DOWN;
+                break;
+        }
+    }
 }
